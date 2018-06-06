6a64d4b19f39c99eec25f467f35179e86a508d5c
dacup72 <dacup72@gmail.com>
2018-06-05 18:05:03 -0600

    Added routes and database to app

---
+++ routes/api/nyt.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const articleController = require("../../controllers/nytController");
+
+
+router
+  .route("/")
+  .get(articleController.findAll);
+
+module.exports = router;
